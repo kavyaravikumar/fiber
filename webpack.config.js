90dc84f43a3eab7995d5fe1f211f6ce6202b2000
Kavya Ravikumar <kara8075@colorado.edu>
2016-04-01 16:01:28 -0600

    Annotations done

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,62 @@
 var path = require('path');
 var webpack = require('webpack');
 
+//exports the configuration object
 module.exports = {
-  devtool: 'eval',
+  //a developer tool to improve the debugging
+  devtool: 'eval', 
+  //This is the entry point for the bundle
   entry: {
+    //Each value in this array will be loaded as a module at startup
     app : [
       'webpack-dev-server/client?http://localhost:3000',
       'webpack/hot/only-dev-server',
       './lib/index.js'],
   },
+  //This will decide how the compiled files are written to the disk
   output: {
+    //Absolute path of the output directory
     path: path.join(__dirname, './public/js/'),
+    //Name of the output file
     filename: `app.js`,
+    //Specifies the publuc URL address which can be accessed in a browser
     publicPath: '/js/'
   },
+  //Additional plugins to be included
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
+  // includes polyfills for different node packages
+  // polyfill -- additional code that provides facilities not built into browsers
   node: {
+    // empties file system
     fs: "empty"
   },
+  //Options which affect the way modules are resolved
   resolve: {
+    //Modules can be replaced with other modules using this option
     alias: {
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    //The list of extensions that help in resolving the modules
     extensions: ['', '.js']
   },
+  //
   resolveLoader: {
     'fallback': path.join(__dirname, 'node_modules')
   },
+  //Options affecting the modules
   module: {    
+    //Loaders that are applied automatically
     loaders: [
     {
+      //Files should have the extension .js
       test: /\.js$/,
+      //List of all the loaders
       loaders: ['react-hot', 'babel'],
+      //Exclude the specified folder
       exclude: /node_modules/,
+      //Include the specified folder in the path
       include: [path.join(__dirname,'./lib')]
     },
     {
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,45 +1,81 @@
+// includes path module
 var path = require('path');
+​
+// includes webpack module
 var webpack = require('webpack');
-
+​
+// exports module according to specifications inside
 module.exports = {
+  // entry -- entry point for module bundle
   entry: {
+    // when an object is passed into entry, multiple entry bundles are created
     app : [
+      // specifies location of app module
       './lib/index.js'],
   },
+  // output -- options affecting compilation output (tells Webpack how to write files to disk)
   output: {
+    // location files are written to
     path: path.join(__dirname, './public/js/'),
+​
+    // specifies output name
     filename: `app.js`,
+​
+    // specifies public URL address of output files when referenced in browser
     publicPath: '/js/'
   },
+  // plugins -- custom build steps
   plugins: [
+    // creates a new dependency injection
     new webpack.DefinePlugin({
         'process.env': {
+          // React is in development mode by default, so this allows React to be used
+          // in production modes
           'NODE_ENV': JSON.stringify('production')
         }
     }),
+    // compresses JavaScript chunk output, removes extra unnecessary development code
     new webpack.optimize.UglifyJsPlugin({
     }),
   ],
+  // includes polyfills for different node packages
+  // polyfill -- additional code that provides facilities not built into browsers
   node: {
+    // empties file system
     fs: "empty"
   },
+  // resolve -- options affecting resolving of modules 
   resolve: {
+    // replaces modules with other modules (in this case react)
     alias: {
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    // extensions used to resolve modules
     extensions: ['', '.js']
   },
+  // resolveLoader -- options affecting resolving of loaders
   resolveLoader: {
+    // directory that webpack should look for modules not found
     'fallback': path.join(__dirname, 'node_modules')
   },
+  // module -- options affecting normal modules
   module: {
+    // automatically applied loaders
     loaders: [
     {
+      // must meet this test condition
       test: /\.js$/,
+​
+      // uses loader modules react-hot & babel
       loaders: ['react-hot', 'babel'],
+​
+      // excludes directory
       exclude: /node_modules/,
+​
+      // match file extension
       include: [path.join(__dirname,'./lib')]
     },
+    // the rest are pretty self-explanatory based on the comments above
     {
       test: /\.xml$/,
       loader: "raw"
